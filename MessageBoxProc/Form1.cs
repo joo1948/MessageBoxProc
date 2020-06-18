f6052db0cc98a1013fb2a569395feea5cee96ed1
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-06-18 14:11:33 +0900

    초기 커밋.

---
+++ MessageBoxProc/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxProc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용","제목");
+
+            DialogResult result;
+
+            result = MessageBox.Show("내용", "제목",MessageBoxButtons.AbortRetryIgnore,
+                MessageBoxIcon.Exclamation);
+
+            
+        }
+    }
+}
